68fa7a3ecc94357483f4c97de07394d481b0bd14
Suraj Karki <suraj.karki500@gmail.com>
2020-06-09 07:32:17 +0545

    Finished  activationController

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -95,3 +95,47 @@ exports.registerController = (req, res) => {
     });
   }
 };
+
+
+exports.activationController = (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+      if (err) {
+        console.log('Activation error');
+        return res.status(401).json({
+          errors: 'Expired link. Signup again'
+        });
+      } else {
+        const { name, email, password } = jwt.decode(token);
+
+        console.log(email);
+        const user = new User({
+          name,
+          email,
+          password
+        });
+
+        user.save((err, user) => {
+          if (err) {
+            console.log('Save error', errorHandler(err));
+            return res.status(401).json({
+              errors: errorHandler(err)
+            });
+          } else {
+            return res.json({
+              success: true,
+              message: user,
+              message: 'Signup success'
+            });
+          }
+        });
+      }
+    });
+  } else {
+    return res.json({
+      message: 'error happening please try again'
+    });
+  }
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 // load controllers
 const {
-    registerController
+    registerController,
+    activationController
 } = require('../controllers/auth.controller.js');
 
-router.post('/register', registerController)
+router.post('/register', registerController);
+router.post('/activation', activationController)
 
 module.exports = router;
