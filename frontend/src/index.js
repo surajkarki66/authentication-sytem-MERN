907b3c72ae3eda57fe4f9c82edb57761f5ec3e98
Suraj Karki <suraj.karki500@gmail.com>
2020-06-10 12:04:01 +0545

    Finished Register

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import App from './App.js';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Login from './screens/Login';
+import Register from './screens/Register';
+
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path='/' exact render={props => <App {...props} />} />
       <Route path='/login' exact render={props => <Login {...props} />} />
+      <Route path='/register' exact render={props => <Register {...props} />} />
       <Redirect to='/' />
     </Switch>
   </BrowserRouter>,
