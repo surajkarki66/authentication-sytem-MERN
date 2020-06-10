81351355c2c5dd266f4a6cb8e231661a9d17a60f
Suraj Karki <suraj.karki500@gmail.com>
2020-06-10 11:50:45 +0545

    Finished Login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import App from './App.js';
+import 'react-toastify/dist/ReactToastify.css';
 
+import Login from './screens/Login';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' exact render={props => <App {...props} />} />
+      <Route path='/login' exact render={props => <Login {...props} />} />
+      <Redirect to='/' />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
